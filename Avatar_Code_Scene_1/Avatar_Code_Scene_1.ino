3a198c08b6dd882e1e48ae3d0c52ac3d91cd224f
DDayton8 <domdayton8@gmail.com>
2024-11-18 01:45:15 -0600

    Add files via upload
    Servo - spins 180 when the copper button is activated, also turns on LED light

---
+++ Avatar_Code_Scene_1/Avatar_Code_Scene_1.ino
@@ -0,0 +1,24 @@
+#include <Servo.h>
+
+Servo myservo;
+const int mySwitchPin = 10;
+bool myBool = false;
+
+void setup() {
+  pinMode(mySwitchPin, INPUT);
+  myservo.attach(8);
+   pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  if (digitalRead(mySwitchPin)==HIGH){
+    myBool = true;
+  }
+
+  if(myBool == true){
+    myservo.write(180);
+    digitalWrite(LED_BUILTIN, HIGH);
+  } else {
+    digitalWrite(LED_BUILTIN, LOW);
+  }
+}
