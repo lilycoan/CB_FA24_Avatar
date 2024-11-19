47892b544670a589c6635363d97688195cae99b1
DDayton8 <domdayton8@gmail.com>
2024-11-18 18:20:37 -0600

    Update Avatar_Code_Scene_1.ino

--- Avatar_Code_Scene_1/Avatar_Code_Scene_1.ino
+++ Avatar_Code_Scene_1/Avatar_Code_Scene_1.ino
@@ -2,24 +2,27 @@
 
 Servo myservo;
 const int mySwitchPin = 10;
-const int ledPin = 7;
+//const int ledPin = 7;
 bool myBool = false;
 
 void setup() {
   pinMode(mySwitchPin, INPUT);
   myservo.attach(8);
-  pinMode(LED_BUILTIN, OUTPUT);
+  //pinMode(LED_BUILTIN, OUTPUT);
+ 
 }
 
 void loop() {
   if (digitalRead(mySwitchPin)==HIGH){
     myBool = true;
+    digitalWrite(LED_BUILTIN, HIGH);
   }
 
   if(myBool == true){
     myservo.write(180);
-    digitalWrite(LED_BUILTIN, HIGH);
+    //digitalWrite(LED_BUILTIN, HIGH);
   } else {
-    digitalWrite(LED_BUILTIN, LOW);
+    myservo.write(0);
+    //digitalWrite(LED_BUILTIN, LOW);
   }
 }
